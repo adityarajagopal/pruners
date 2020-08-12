b1a956af22893e2fd4b5090c0fca7e56c0c3ce48
adityarajagopal <ar4414@ic.ac.uk>
2020-08-12 14:20:16 +0100

    making pruners it's own repo as multiple projects need it

---
+++ alexnet.py
@@ -0,0 +1,28 @@
+import sys
+import csv
+import os
+import numpy as np
+import time
+from tqdm import tqdm
+import json
+import pickle
+import subprocess
+import importlib
+import math
+import copy
+
+from src.ar4414.memory_model.pruners.base import BasicPruning
+
+import torch
+import torch.nn as nn
+
+class AlexNetPruning(BasicPruning):
+#{{{
+    def __init__(self, params, model):
+    #{{{
+        self.fileName = 'alexnet_{}.py'.format(int(params.samplePruning['pruning_perc']))
+        self.netName = 'AlexNet'
+        
+        super().__init__(params, model)
+    #}}} 
+#}}}
