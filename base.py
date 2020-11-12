89a88247efb6ab2c6f3dd14cfafafc8f7bcdd68b
adityarajagopal <ar4414@ic.ac.uk>
2020-11-12 19:12:06 +0000

    removed old code

--- base.py
+++ base.py
@@ -146,45 +146,6 @@ class BasicPruning(ABC):
         minIdx = np.argmin(array[np.nonzero(array)]) 
         return (minIdx, array[minIdx])     
     
-    def inc_prune_rate_old(self, layerName):
-    #{{{
-        lParam = str(layerName)
-        # remove 1 output filter from current layer
-        self.layerSizes[lParam][0] -= 1 
-        currLayerSize = self.layerSizes[lParam]
-        paramsPruned = currLayerSize[1]*currLayerSize[2]*currLayerSize[3]
-        
-        nextLayerDetails = self.depBlock.linkedConvAndFc[lParam]
-        for (nextLayer, groups) in nextLayerDetails:
-            nextLayerSize = self.layerSizes[nextLayer]
-            # currLayerSize = self.layerSizes[lParam]
-            # paramsPruned = currLayerSize[1]*currLayerSize[2]*currLayerSize[3]
-
-            # check if FC layer
-            if len(nextLayerSize) == 2: 
-                # paramsPruned += nextLayerSize[0]
-                # self.layerSizes[nextLayer][1] -= 1 
-                
-                fcParamsPruned = int(nextLayerSize[1] / (currLayerSize[0] + 1))
-                paramsPruned += fcParamsPruned * nextLayerSize[0]
-                # self.layerSizes[nextLayer][1] = currLayerSize[0] * fcParamsPruned
-                self.layerSizes[nextLayer][1] -= fcParamsPruned
-            else:
-                #TODO:
-                # this is assuming we only have either non-grouped convolutions or dw convolutions
-                # have to change to accomodate grouped convolutions
-                nextOpChannels = nextLayerSize[0] if groups == 1 else 1
-                paramsPruned += nextOpChannels*nextLayerSize[2]*nextLayerSize[3]
-                
-                # remove 1 input activation from next layer if it is not a dw conv
-                if groups == 1:
-                    self.layerSizes[nextLayer][1] -= 1
-            
-        self.currParams -= paramsPruned
-        
-        return (100. * (1. - (self.currParams / self.totalParams)))
-    #}}}
-    
     def inc_prune_rate(self, layerName):
     #{{{
         lParam = str(layerName)
